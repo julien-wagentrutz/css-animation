8d55179322cff40330d42d8b9a2c87fa9f51090b
julien Wagentrutz <julien.wagentrutz@hetic.net>
2019-11-02 19:12:18 +0100

    Ajout de la tour eiffel

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const player = document.querySelector('.player');
 const planet = document.querySelector('.planet .flowers');
+const road = document.querySelector('.planet .road');
+const lines = document.querySelectorAll('.planet .line');
+const trees = document.querySelectorAll('.tree')
+const houses = document.querySelectorAll('.house')
+const menuButton = document.querySelector('.button-open')
+const menuElement = document.querySelector('.menu')
+const sadButton = document.querySelector('.button-sad')
 let state = 'motionless'
 let testJump = true
 let degEarth = 0
 let dir = true
-step = 1
+let step = 1
+
+/* MENU */
+
+menuButton.addEventListener('click',
+    function () {
+    menuElement.classList.toggle('open')
+    })
+
+
+/* INTERACTION */
 
 window.addEventListener('keydown',
     function (event) {
-        if(step%2 == 0)
-        {
-            if (dir) {
-                degEarth += 5
-            }
-            else {
-                degEarth -= 5
-            }
-            planet.style.transform = "rotate(" + degEarth + "deg)"
-        }
+
         if(event.code == "ArrowRight")
         {
             player.classList.remove('run-pause')
@@ -25,6 +33,17 @@ window.addEventListener('keydown',
             state = 'run-pause'
             player.style.transform = "rotateY(0deg)"
             step++
+            if(step%2 == 0)
+            {
+                if (dir) {
+                    degEarth += 5
+                }
+                else {
+                    degEarth -= 5
+                }
+                planet.style.transform = "rotate(" + degEarth + "deg)"
+                road.style.transform = "rotate(" + degEarth + "deg)"
+            }
 
         }
         else if(event.code == "ArrowLeft")
@@ -33,8 +52,18 @@ window.addEventListener('keydown',
             dir = true
             state = 'run-pause'
             player.style.transform = "rotateY(180deg)"
-            step++
-
+            step--
+            if(step%2 == 0)
+            {
+                if (dir) {
+                    degEarth += 5
+                }
+                else {
+                    degEarth -= 5
+                }
+                planet.style.transform = "rotate(" + degEarth + "deg)"
+                road.style.transform = "rotate(" + degEarth + "deg)"
+            }
         }
         else if(event.code == "Space" && testJump)
         {
@@ -46,10 +75,6 @@ window.addEventListener('keydown',
                 clearInterval(inter)
             },1000)
 
-            if(degEarth%360 == 0)
-            {
-                console.log('rgsdfeiuu')
-            }
         }
         else
         {
@@ -61,6 +86,52 @@ window.addEventListener('keydown',
             state = 'motionless'
         }
 
+
+        if(step >= 100)
+        {
+            for (let i = 0; i < trees.length;i++)
+            {
+                trees[i].classList.add('appear')
+            }
+        }
+        else if(step < 100)
+        {
+            for (let i = 0; i < trees.length;i++)
+            {
+                trees[i].classList.remove('appear')
+            }
+        }
+
+        if(step >= 200)
+        {
+            for (let i = 0; i < houses.length;i++)
+            {
+                houses[i].classList.add('appear')
+            }
+        }
+        else if(step < 200)
+        {
+            for (let i = 0; i < houses.length;i++)
+            {
+                houses[i].classList.remove('appear')
+            }
+        }
+        if(step >= 250)
+        {
+            road.classList.add('appear')
+            for (let i = 0; i < lines.length;i++)
+            {
+                lines[i].classList.add('appear')
+            }
+        }
+        else if(step < 250)
+        {
+            road.classList.remove('appear')
+            for (let i = 0; i < lines.length;i++)
+            {
+                lines[i].classList.remove('appear')
+            }
+        }
 });
 
 
