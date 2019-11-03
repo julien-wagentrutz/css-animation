c469b394bbed5b14cb60505b7a121539d81b4e5d
julien Wagentrutz <julien.wagentrutz@hetic.net>
2019-11-03 14:41:56 +0100

    Ajout du saut et de la chute des pommes

--- script.js
+++ script.js
@@ -1,146 +1,344 @@
-const player = document.querySelector('.player');
-const planet = document.querySelector('.planet .flowers');
-const road = document.querySelector('.planet .road');
-const lines = document.querySelectorAll('.planet .line');
-const trees = document.querySelectorAll('.tree')
-const houses = document.querySelectorAll('.house')
-const menuButton = document.querySelector('.button-open')
-const menuElement = document.querySelector('.menu')
-const sadButton = document.querySelector('.button-sad')
-let state = 'motionless'
-let testJump = true
-let degEarth = 0
-let dir = true
-let step = 1
+// CONSTANTES
 
-/* MENU */
+    //contant of the world
+const player = document.querySelector('.js-player');
+const planet = document.querySelector('.js-flowers');
+const road = document.querySelector('.js-road');
+const trees = document.querySelectorAll('.js-tree')
+const houses = document.querySelectorAll('.js-house')
+const buildings = document.querySelectorAll('.js-building')
+const eiffelTower = document.querySelector('.js-eiffel-tower')
+const treesDeg =
+    {
+        '-356' : 0,
+        '-358' : 0,
+        '-0'   : 0,
+        '-2'   : 0,
+        '-4'   : 0,
+        '-110' : 2,
+        '-112' : 2,
+        '-114' : 2,
+        '-116' : 2,
+        '-192' : 4,
+        '-194' : 4,
+        '-196' : 4,
+        '-198' : 4
+    }
 
-menuButton.addEventListener('click',
-    function () {
-    menuElement.classList.toggle('open')
-    })
+    // constant of the menu
+
+const menuButton = document.querySelector('.js-button-open')
+const menuElement = document.querySelector('.js-menu')
+const sadButton = document.querySelector('#sad')
+const danceButton = document.querySelector('#dance')
+const treeButton = document.querySelector('#tree')
+const houseButton = document.querySelector('#house')
+const roadButton = document.querySelector('#road')
+const buildingButton = document.querySelector('#building')
+const eiffelButton = document.querySelector('#eiffel-tower')
+const speedButton = document.querySelector('#speed-player')
+
+// VARIABLES
+
+let earthDeg = 0;
+let step = 0;
+let direction = true;
+let playerStep = 0
+let speedPlayer = 2
+let unCheck = false
 
 
-/* INTERACTION */
+    // function
 
-window.addEventListener('keydown',
-    function (event) {
+/*
+    function movePlayer
+    param :
+    descrip :
+        Move the player and the planet in function of which direction it go
+*/
 
-        if(event.code == "ArrowRight")
+
+const movePlayer = () =>
+{
+    playerStep++
+
+    if(direction)
+    {
+        player.style.transform = "rotateY(0deg)"
+        earthDeg -= speedPlayer
+    }
+    else
+    {
+        player.style.transform = "rotateY(180deg)"
+        earthDeg += speedPlayer
+    }
+    player.classList.remove('run-pause')
+    planet.style.transform = "rotate(" + earthDeg + "deg)"
+
+    if(playerStep >= 50)
+    {
+        if(direction)
         {
-            player.classList.remove('run-pause')
-            dir = false
-            state = 'run-pause'
-            player.style.transform = "rotateY(0deg)"
             step++
-            if(step%2 == 0)
-            {
-                if (dir) {
-                    degEarth += 5
-                }
-                else {
-                    degEarth -= 5
-                }
-                planet.style.transform = "rotate(" + degEarth + "deg)"
-                road.style.transform = "rotate(" + degEarth + "deg)"
-            }
-
-        }
-        else if(event.code == "ArrowLeft")
-        {
-            player.classList.remove('run-pause')
-            dir = true
-            state = 'run-pause'
-            player.style.transform = "rotateY(180deg)"
-            step--
-            if(step%2 == 0)
-            {
-                if (dir) {
-                    degEarth += 5
-                }
-                else {
-                    degEarth -= 5
-                }
-                planet.style.transform = "rotate(" + degEarth + "deg)"
-                road.style.transform = "rotate(" + degEarth + "deg)"
-            }
-        }
-        else if(event.code == "Space" && testJump)
-        {
-            testJump = false
-            player.classList.add('jump');
-            let inter = setInterval(function () {
-                player.classList.remove('jump')
-                testJump = true
-                clearInterval(inter)
-            },1000)
-
+            if(step>5)step=5
         }
         else
         {
-            if(state != 'motionless' )
+            step--
+            if(step<0)step=0
+        }
+        unCheck = false
+        playerStep = 0
+    }
+
+    modifyWorld()
+}
+
+/*
+    function stopMovePlayer
+    param :
+    descrip :
+        Stop the animation of run of the player
+*/
+
+
+const stopMovePlayer = () =>
+{
+    player.classList.add('run-pause')
+}
+
+/*
+    function modifyWorld
+    param :
+    descrip :
+        appear or disapear element about the step of the player
+*/
+
+const modifyWorld = ()=>
+{
+    switch (step)
+    {
+        case 0:
+            if(!unCheck)
+                appearElement(treeButton,trees,true)
+            break;
+        case  1:
+            if(!unCheck)
+             appearElement(treeButton,trees)
+            appearElement(houseButton,houses,true)
+            break;
+        case  2:
+            if(!unCheck)
+                appearElement(houseButton,houses)
+            appearElement(roadButton,road,true)
+            break;
+        case  3:
+            if(!unCheck)
+                appearElement(roadButton,road)
+            appearElement(buildingButton,buildings,true)
+            break;
+        case  4:
+            if(!unCheck)
+                appearElement(buildingButton,buildings)
+            appearElement(eiffelButton,eiffelTower,true)
+            break;
+        case  5:
+            if(!unCheck)
+                appearElement(eiffelButton,eiffelTower)
+            break;
+    }
+}
+
+/*
+    function appearElement
+    param :
+            button => DOM => button on click
+            element => DOM => element affect
+            disapear => boolean => if is appear (false) or disapear (true)
+    descrip :
+            appear or disapear element, and checked the checkbox
+ */
+const appearElement = (button,element,disapear) =>
+{
+    if(!button.checked)
+    {
+        if(element.length > 1)
+        {
+            for(let i = 0; i < element.length; i++ )
             {
-                player.classList.add(state)
+                if(disapear)
+                    element[i].classList.remove('appear')
+                else
+                    element[i].classList.add('appear')
 
             }
-            state = 'motionless'
         }
-
-
-        if(step >= 100)
+        else
         {
-            for (let i = 0; i < trees.length;i++)
+            if(disapear)
+                element.classList.remove('appear')
+            else
+                element.classList.add('appear')
+        }
+    }
+    button.checked = !disapear
+}
+
+
+/*
+    function jump
+    param:
+    description : make the player jump and test if he jump on apple
+ */
+
+const jump = () =>
+{
+    if(treeButton.checked)
+    {
+        for(var key in treesDeg)
+        {
+            if(key == earthDeg%360)
             {
-                trees[i].classList.add('appear')
+                let apples = trees[treesDeg[key]].querySelectorAll('.apple:not(.fall)')
+                if(apples.length>0)
+                {
+                   let rand = Math.floor(Math.random()*apples.length)
+                    apples[rand].classList.add('fall')
+                    let top = apples[rand].offsetTop - 3
+                   apples[rand].style.transform = 'translateY(-'+ top +'px)'
+                }
             }
         }
-        else if(step < 100)
-        {
-            for (let i = 0; i < trees.length;i++)
-            {
-                trees[i].classList.remove('appear')
-            }
-        }
-
-        if(step >= 200)
-        {
-            for (let i = 0; i < houses.length;i++)
-            {
-                houses[i].classList.add('appear')
-            }
-        }
-        else if(step < 200)
-        {
-            for (let i = 0; i < houses.length;i++)
-            {
-                houses[i].classList.remove('appear')
-            }
-        }
-        if(step >= 250)
-        {
-            road.classList.add('appear')
-            for (let i = 0; i < lines.length;i++)
-            {
-                lines[i].classList.add('appear')
-            }
-        }
-        else if(step < 250)
-        {
-            road.classList.remove('appear')
-            for (let i = 0; i < lines.length;i++)
-            {
-                lines[i].classList.remove('appear')
-            }
-        }
-});
+    }
 
 
 
+    player.classList.add('jump');
+    let inter = setInterval(function () {
+        player.classList.remove('jump')
+        clearInterval(inter)
+    },1000)
+}
 
-window.addEventListener('keyup',
-    function (event) {
-        if(state != 'motionless' && event.code != 'Space')
+
+/* MENU */
+
+menuButton.addEventListener(
+    'click',
+     () =>
+    {
+    menuElement.classList.toggle('open')
+    }
+)
+
+
+
+// TOGGLE sad class to the head of the player
+sadButton.addEventListener(
+    'click', () => {player.querySelector('.head .mouth').classList.toggle('sad')}
+)
+
+speedButton.addEventListener(
+    'click', () => {speedPlayer = parseInt(speedButton.value)}
+)
+
+// TOGGLE dance class to the head of the player
+
+danceButton.addEventListener(
+    'click', () => {player.classList.toggle('dance')}
+)
+
+// Show the road on the world via the menu button
+
+roadButton.addEventListener(
+    'click', () =>
+    {
+        if(step == 3 )unCheck = true
+        road.classList.toggle('appear')
+    }
+)
+
+// Show the Eiffel Tower on the world via the menu button
+
+eiffelButton.addEventListener(
+    'click', () =>
+    {
+        if(step == 5 )unCheck = true
+        eiffelTower.classList.toggle('appear')
+    }
+)
+
+
+
+// Show the trees on the world via the menu button
+
+treeButton.addEventListener(
+    'click', () =>
+    {
+        if(step == 1 )unCheck = true
+        for(let i = 0; i < trees.length; i++ )
         {
-            player.classList.add(state)
+            trees[i].classList.toggle('appear')
         }
- });
+    }
+)
+
+// Show the house on the world via the menu button
+
+houseButton.addEventListener(
+    'click', () =>
+    {
+        if(step == 2 )unCheck = true
+        for(let i = 0; i < houses.length; i++ )
+        {
+            houses[i].classList.toggle('appear')
+        }
+    }
+)
+
+// Show the building on the world via the menu button
+
+buildingButton.addEventListener(
+    'click', () =>
+    {
+        if(step == 4 )unCheck = true
+        for(let i = 0; i < buildings.length; i++ )
+        {
+            buildings[i].classList.toggle('appear')
+        }
+    }
+)
+
+
+/* INTERACTION WORLD */
+
+    // player action with keyboard
+
+window.addEventListener(
+    'keydown', (event) =>
+    {
+        if(event.code === "ArrowRight")
+        {
+            direction = true
+            movePlayer()
+        }
+        else if(event.code === "ArrowLeft")
+        {
+            direction = false
+           movePlayer()
+        }
+        else if (event.code === "Space")
+        {
+            jump()
+        }
+    }
+);
+
+// Stop animation when player don't move
+window.addEventListener(
+    'keyup', (event) =>
+    {
+        stopMovePlayer()
+
+    }
+);
